431d03340d89da22e698c99000d071b3b7e0135e
l0v3t00th <kimz230@newschool.edu>
2025-12-21 01:05:03 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-  const box = document.getElementById('box');
-  window.addEventListener('mousemove', (e) => 
-    {const x = e.clientX;
-    const y = e.clientY;
-    box.style.left = x + '9999px';
-    box.style.top  = y + '9999px';
-     });
